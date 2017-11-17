531269e9d36abe3f3c73601e2bf7f58c68cabc84
Sean King <kingsc@miamioh.edu>
2017-11-17 18:47:10 -0500

    refactor(Components): Import/Export from base index file

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Bar } from './Bar'
+export { default as Chart } from './Chart'
+export { default as DataList } from './DataList'
+export { default as DataListItem } from './DataListItem'
+export { default as Gradient } from './Gradient'
+export { default as GradientCircle } from './GradientCircle'
+export { default as LoginForm } from './LoginForm'
+export { default as MoodLogger } from './MoodLogger'
