24ab850bd773f53ba323cfd54318bacfbc8d48c5
Sean King <kingsc@miamioh.edu>
2017-11-17 18:44:31 -0500

    feat(GradientCircle): Add GradientCircle component

---
+++ src/components/GradientCircle.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Svg, { Circle } from 'react-native-svg'
+import Gradient from './Gradient'
+
+const GradientCircle = ({
+  topColor,
+  bottomColor,
+  size,
+  strokeWidth = 2.5
+}) => (
+  <Svg
+    width={size}
+    height={size}
+  >
+    <Gradient
+      id='linearGradient'
+      topColor={topColor}
+      bottomColor={bottomColor}
+    />
+    <Circle
+      cx={size / 2}
+      cy={size / 2}
+      r={(size / 2) - (strokeWidth * 2)}
+      stroke='url(#linearGradient)'
+      strokeWidth={strokeWidth}
+      fill='transparent'
+    />
+  </Svg>
+)
+
+export { GradientCircle as default }
