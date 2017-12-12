c6ade353ef124214f3268bdc8f9c7d8544a759c7
Sean King <kingsc@miamioh.edu>
2017-12-12 18:20:50 -0500

    feat(ChartActions): Add steps_fetch_success and sleep_fetch_success actions

--- src/actions/ChartActions.js
+++ src/actions/ChartActions.js
@@ -1,8 +1,31 @@
+import AppleHealthKit from 'rn-apple-healthkit'
 import {
-  UPDATE_COMPARED_ID
+  UPDATE_COMPARED_ID,
+  STEPS_FETCH_SUCCESS,
+  SLEEP_FETCH_SUCCESS
 } from './types'
 
 export const updateComparedId = comparedId => ({
   type: UPDATE_COMPARED_ID,
   comparedId
 })
+
+export const sleepFetch = () => {
+  const weekInMs = 518400000
+  const aWeekAgo = new Date(new Date().getTime() - weekInMs)
+  const options = {
+    startDate: aWeekAgo.toISOString(),
+    endDate: (new Date()).toISOString()
+  }
+
+  return (dispatch) => {
+    AppleHealthKit.getSleepSamples(options, (err: Object, results: Array<Object>) => {
+      if (err) {
+        console.log('Error fetching sleep data from HK: ', err) // eslint-disable-line no-console
+        return
+      }
+      console.log(results) // eslint-disable-line no-console
+      dispatch({ type: SLEEP_FETCH_SUCCESS, payload: results })
+    })
+  }
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,10 +1,17 @@
+// Auth
 export const EMAIL_CHANGED = 'email_changed'
 export const PASSWORD_CHANGED = 'password_changed'
 export const LOGIN_USER_SUCCESS = 'login_user_success'
 export const LOGIN_USER_FAIL = 'login_user_fail'
 export const LOGIN_USER = 'login_user'
 
+// Log
 export const UPDATE_SLIDER = 'update_slider'
 export const LOG_MOOD_SUCCESS = 'log_mood_success'
+export const MOODS_FETCH = 'moods_fetch'
 export const MOODS_FETCH_SUCCESS = 'moods_fetch_success'
+
+// Chart
 export const UPDATE_COMPARED_ID = 'update_compared_id'
+export const STEPS_FETCH_SUCCESS = 'steps_fetch_success'
+export const SLEEP_FETCH_SUCCESS = 'sleep_fetch_success'
