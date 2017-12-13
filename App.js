46a9ab24103a15375452714263e898ce150f3497
Sean King <kingsc@miamioh.edu>
2017-12-13 17:02:28 -0500

    feat(App): Add redux-promise middleware

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import { Provider } from 'react-redux'
 import ReduxThunk from 'redux-thunk'
+import promiseMiddleware from 'redux-promise'
 import firebase from 'firebase'
 import reducers from './src/reducers'
 import Router from './src/Router'
 
-const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(ReduxThunk)))
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(ReduxThunk, promiseMiddleware)))
 
 class App extends Component {
   componentWillMount() {
