d1a977fbf71796714d52099ad693efcb2bf2387b
Sean King <kingsc@miamioh.edu>
2017-12-13 17:09:51 -0500

    feat(ChartActions): Add stepsFetchWeek action

--- src/actions/ChartActions.js
+++ src/actions/ChartActions.js
@@ -4,15 +4,41 @@ import {
   STEPS_FETCH_SUCCESS,
   SLEEP_FETCH_SUCCESS
 } from './types'
+import { createDateArray } from '../utility'
+
+const weekInMs = 518400000
+const aWeekAgo = new Date(new Date().getTime() - weekInMs)
 
 export const updateComparedId = comparedId => ({
   type: UPDATE_COMPARED_ID,
   comparedId
 })
 
+export const stepsFetchWeek = () => {
+  const weekOfStepsArr = createDateArray({ steps: 0 })
+  weekOfStepsArr.forEach(entry => {
+    let stepValue = 0
+    const options = {
+      date: entry.fullDate.toISOString()
+    }
+
+    AppleHealthKit.getStepCount(options: Object, (err: Object, results: Object) => {
+      if (err) {
+        console.log('Error fetching step data from HK: ', err) // eslint-disable-line no-console
+        return 0
+      }
+      console.log(results) // eslint-disable-line no-console
+      stepValue = results.value
+      entry.steps = stepValue
+    })
+  })
+
+  return (dispatch) => {
+    dispatch({ type: STEPS_FETCH_SUCCESS, payload: weekOfStepsArr })
+  }
+}
+
 export const sleepFetch = () => {
-  const weekInMs = 518400000
-  const aWeekAgo = new Date(new Date().getTime() - weekInMs)
   const options = {
     startDate: aWeekAgo.toISOString(),
     endDate: (new Date()).toISOString()
@@ -24,7 +50,6 @@ export const sleepFetch = () => {
         console.log('Error fetching sleep data from HK: ', err) // eslint-disable-line no-console
         return
       }
-      console.log(results) // eslint-disable-line no-console
       dispatch({ type: SLEEP_FETCH_SUCCESS, payload: results })
     })
   }
