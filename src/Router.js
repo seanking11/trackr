efbc524d61c2d07112df58eff4b59c7651b1056d
Sean King <kingsc@miamioh.edu>
2017-12-15 17:11:07 -0600

    feat(Navigation): Move navigation to bottom and add tabs

--- src/Router.js
+++ src/Router.js
@@ -1,42 +1,97 @@
-import React from 'react'
-import { Scene, Router, Actions } from 'react-native-router-flux'
+import React, { Component } from 'react'
+import { View, Text, StatusBar } from 'react-native'
+import { Scene, Router } from 'react-native-router-flux'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import LoginForm from './components/LoginForm'
 import MoodLogger from './components/MoodLogger'
 import ChartPage from './components/ChartPage'
 import Colors from './components/common/Colors'
 
-const RouterComponent = () => (
-  <Router
-    sceneStyle={{
-      backgroundColor: Colors.backgroundGray
-    }}
-  >
-    <Scene key='root' hideNavBar>
-      <Scene key='auth'>
-        <Scene
-          key='login'
-          component={LoginForm}
-          title='Login'
-        />
-      </Scene>
+const TabIcon = ({ focused, title, iconName }) => {
+  const color = focused ? '#24e98a' : '#DDD'
+  const styles = {
+    viewStyles: {
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      alignSelf: 'center',
+      justifyContent: 'center'
+    },
+    iconStyles: {
+      color
+    },
+    textStyles: {
+      color,
+      fontSize: 12
+    }
+  }
 
-      <Scene key='main'>
-        <Scene
-          initial
-          key='log'
-          component={MoodLogger}
-          title='Log Mood'
-          rightTitle='Chart'
-          onRight={() => Actions.chart()}
-        />
-        <Scene
-          key='chart'
-          component={ChartPage}
-          title='Chart'
-        />
-      </Scene>
-    </Scene>
-  </Router>
-)
+  return (
+    <View style={styles.viewStyles}>
+      <Icon
+        style={styles.iconStyles}
+        name={iconName || 'circle'}
+        size={18}
+      />
+      <Text
+        style={styles.textStyles}
+      >
+        {title}
+      </Text>
+    </View>
+  )
+}
+
+class RouterComponent extends Component {
+  componentWillMount() {
+    StatusBar.setBarStyle('light-content', true)
+  }
+
+  render() {
+    return (
+      <Router
+        sceneStyle={{ backgroundColor: Colors.backgroundGray }}
+      >
+        <Scene key='root' hideNavBar>
+          <Scene key='auth'>
+            <Scene
+              key='login'
+              component={LoginForm}
+              title='Login'
+            />
+          </Scene>
+
+          <Scene
+            key='main'
+            tabs
+            tabBarStyle={{
+              backgroundColor: Colors.cardBackground
+            }}
+            statusBarStyle='light-content'
+            showLabel={false}
+          >
+            <Scene
+              initial
+              hideNavBar
+              key='log'
+              component={MoodLogger}
+              title='Log Mood'
+              icon={TabIcon}
+              iconName='smile-o'
+            />
+            <Scene
+              key='chart'
+              hideNavBar
+              component={ChartPage}
+              title='Chart'
+              icon={TabIcon}
+              iconName='bar-chart-o'
+            />
+          </Scene>
+        </Scene>
+      </Router>
+    )
+  }
+}
 
 export default RouterComponent
--- src/components/ChartPage.js
+++ src/components/ChartPage.js
@@ -4,7 +4,7 @@ import Chart from './Chart'
 import CompareList from './CompareList'
 
 const ChartPage = () => (
-  <View style={{ flex: 1 }}>
+  <View style={{ flex: 1, marginTop: 40 }}>
     <Chart />
     <CompareList />
   </View>
